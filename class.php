3d8deed3ffb23ac13d62ecbfece952d8d61fdcc7
funshofatoye <fatoyefunsho@gmail.com>
2017-11-09 02:15:15 -1200

    frustrated

--- class.php
+++ class.php
@@ -10,6 +10,7 @@
    echo "yes we are ,but pulling and pushing tho";
    echo "always check your spellings,ok ?";
    echo "is your box baked or still cooking"
+   echo "Omo, na die for here o";
 
 
 ?>
